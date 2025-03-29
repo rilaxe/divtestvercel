c82cd72d1e05cc53594894b7841634902c619f7c
Oparaiwu Silas <silas@pixelsgroup.ng>
2025-03-29 16:48:18 +0100

    first commit5

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
+//import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
